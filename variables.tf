5b9830e5982fcfd10ae5e259887ac30fc8390279
dcshiman <shiman.dc@gmail.com>
2020-06-13 20:19:01 +0800

    fix variables

--- variables.tf
+++ variables.tf
@@ -1,43 +1,34 @@
 variable "resources" {
   type = any
+  description = "Resources for the API gateway, see readme"
 }
 
 variable "name" {
   type = string
-}
-
-variable "create" {
-  type = bool
-  default = true
+  description = "Name of the API Gateware"
 }
 
 variable "authorizers" {
   type = any
 
   default = []
-}
-
-variable "domain" {
-  type = string
-  default = "mispace.app"
-}
-
-variable "subdomain" {
-  type = string
-  default = null
+  description = "Authorizers, see reademe"
 }
 
 variable "stage" {
   type = string
   default = "default"
+  description = "Stage of the API Gateway"
 }
 
 variable "logs" {
   type = bool
   default = false
+  description = "Enable or disable logs"
 }
 
 variable "tags" {
   type = map(any)
   default = {}
+  description = "Tags to be used for the gateway"
 }
